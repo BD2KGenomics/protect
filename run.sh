a1b58ebce7c5595d94f19a640ca7affd7298d22e
Drew Thompson <drkthomp@ucsc.edu>
2020-12-07 10:59:03 -0800

    remove extra

--- run.sh
+++
@@ -1,3 +0,0 @@
-source venv/bin/activate
-ProTECT --config mustard_config.yaml --workDir /scratch/drkthomp/workDir /scratch/drkthomp/jobStore --restart|& tee errors/$(date '+%Y-%m-%d-%H-%M-%S').txt
-
